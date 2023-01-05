20377928c6133a426c6746f2b850dc7c5aab1ab1
Simone Taeggi <startae14@gmail.com>
2023-01-05 18:00:58 +0100

    Server Side Rendering done by zero with Express

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, lazy, Suspense } from "react";
-import { createRoot } from "react-dom/client";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 
@@ -25,31 +24,27 @@ const App = () => {
         background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPetHook}>
-            <Suspense
-              fallback={
-                <div className="items-center justify-center flex p-1">
-                  <h2 className="text-8xl animate-spin">🌀</h2>
-                </div>
-              }
-            >
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me!</Link>
-              </header>
-              <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </AdoptedPetContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPetHook}>
+          <Suspense
+            fallback={
+              <div className="items-center justify-center flex p-1">
+                <h2 className="text-8xl animate-spin">🌀</h2>
+              </div>
+            }
+          >
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
