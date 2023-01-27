ae541cf3bf01f76a69a16e4c7f92086554d7c51b
Simone Taeggi <startae14@gmail.com>
2023-01-27 22:09:23 +0100

    initial code

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,23 +2,22 @@ import { Component } from "react";
 
 class Carousel extends Component {
   state = {
-    active: 0, // index first photo
-  }
-
-  static defaultProps = {
-    images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
+    active: 0,
   };
 
-  handleIndexClick = (e) => {
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (event) => {
     this.setState({
-      active: +e.target.dataset.index
-    })
+      active: +event.target.dataset.index,
+    });
   };
 
   render() {
-    const { active } =  this.state;
+    const { active } = this.state;
     const { images } = this.props;
-
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal" />
@@ -36,7 +35,7 @@ class Carousel extends Component {
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,10 @@
 import { useContext, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
-import useBreedList from './useBreedList';
-import fetchSearch from "./fetchSearch";
 import AdoptedPetContext from "./AdoptedPetContext";
-
-const ANIMALS = [ "bird", "cat", "dog", "rabbit", "reptile" ];
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [requestParams, setRequestParams] = useState({
@@ -13,9 +12,9 @@ const SearchParams = () => {
     animal: "",
     breed: "",
   });
+  const [adoptedPet] = useContext(AdoptedPetContext);
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
-  const [adoptedPet] = useContext(AdoptedPetContext);
 
   const results = useQuery(["search", requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
@@ -31,61 +30,53 @@ const SearchParams = () => {
             breed: formData.get("breed") ?? "",
             location: formData.get("location") ?? "",
           };
-          setRequestParams(obj)
+          setRequestParams(obj);
         }}
       >
-        {
-          adoptedPet ? (
-            <div className="pet image-container">
-              <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
-            </div>
-          ) : null // you have to remove this semi-colon, my auto-formatter adds it back if I delete it
-        }
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
-          <input
-            name="location"
-            id="location"
-            placeholder="Location"
-            type="text"
-          />
+          <input id="location" name="location" placeholder="Location" />
         </label>
+
         <label htmlFor="animal">
           Animal
           <select
-            name="animal"
             id="animal"
-            value={animal}
+            name="animal"
             onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+            onBlur={(e) => {
               setAnimal(e.target.value);
             }}
           >
             <option />
-            {
-              ANIMALS.map((item) => (
-                <option key={item}>{item}</option>
-              ))
-            }
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
           </select>
         </label>
+
         <label htmlFor="breed">
           Breed
-          <select
-            id="breed"
-            disabled={breeds.length === 0}
-            name="breed"
-          >
+          <select disabled={!breeds.length} id="breed" name="breed">
             <option />
-            {
-              breeds.map((item) => (
-                <option key={item}>{item}</option>
-              ))
-            }
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
           </select>
         </label>
-        <button>
-          Submit
-        </button>
+
+        <button>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
