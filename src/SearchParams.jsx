b59b9c251543f25bb3ef7bc72babb2ae4db8b43c
Simone Taeggi <startae14@gmail.com>
2023-02-23 18:23:52 +0100

    create another slice to save the Researches

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import useBreedList from "./useBreedList";
 import fetchSearch from "./fetchSearch";
+import { all } from "./searchParamsSlice";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [requestParams, setRequestParams] = useState({
-    location: "",
-    animal: "",
-    breed: "",
-  });
   const adoptedPet = useSelector((state) => state.adoptedPet.value);
+  const searchParams = useSelector((state) => state.searchParams.value);
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
 
-  const results = useQuery(["search", requestParams], fetchSearch);
+  const results = useQuery(["search", searchParams], fetchSearch);
+  const dispatch = useDispatch();
   const pets = results?.data?.pets ?? [];
 
   return (
@@ -30,7 +28,7 @@ const SearchParams = () => {
             breed: formData.get("breed") ?? "",
             location: formData.get("location") ?? "",
           };
-          setRequestParams(obj);
+          dispatch(all(obj));
         }}
       >
         {adoptedPet ? (
