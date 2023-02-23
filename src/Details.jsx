ce58a1ebed201726fe9b4ede7ab7cae0125b5fc0
Simone Taeggi <startae14@gmail.com>
2023-02-23 17:34:43 +0100

    refactor Details to use RTK andd the adoptPetSlice

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,20 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { useContext, useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
 import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import { adopt } from "./adoptedPetSlice";
 
 const Details = () => {
   const { id } = useParams();
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
   const results = useQuery(["details", id], fetchPet);
-  // eslint-disable-next-line no-unused-vars
-  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
 
+  const dispatch = useDispatch();
+  
   if (results.isLoading) {
     return (
       <div className="loading-pane">
@@ -40,7 +41,7 @@ const Details = () => {
               <div className="buttons">
                 <button
                   onClick={() => {
-                    setAdoptedPet(pet);
+                    dispatch(adopt(pet));
                     navigate("/");
                   }}
                 >
